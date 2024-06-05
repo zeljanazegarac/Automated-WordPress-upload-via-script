217069a40b8447d281f9b53796737de9076ba287
Zeljana Zegarac <zeljanazegarac@192.168.154.130>
2024-06-05 12:57:57 +0200

    Step 2: Start and enable NGINX, MariaDB, and PHP-FPM services

--- setup-wordpress.sh
+++ setup-wordpress.sh
@@ -3,3 +3,11 @@
 # Update packages and install NGINX, MariaDB, PHP
 sudo dnf update -y
 sudo dnf install nginx mariadb-server php php-fpm php-mysqlnd -y
+
+# Start and enable services
+sudo systemctl start nginx
+sudo systemctl enable nginx
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+sudo systemctl start php-fpm
+sudo systemctl enable php-fpm
